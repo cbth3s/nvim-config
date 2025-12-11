b5ec1c87f41b4e29f3a707906cf4539827ababb9
cbth3s <vvtf77@gmail.com>
2025-12-11 08:19:14 +0300

    modify lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,30 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup()
-    end,
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'gruvbox',
+        icons_enabled = true,
+        component_separators = { left = '│', right = '│' },
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { { 'mode', icon = '' } },
+        lualine_b = {
+            { 'branch', icon = '' },
+            { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } },
+            { 'diagnostics', symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' } }
+        },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = {},
+      },
+      inactive_sections = {
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+      },
+    })
+  end,
 }
